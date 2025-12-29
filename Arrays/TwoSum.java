e05b2751ee0d91f14aa1cc3f9d0e92744a1c336a
AbhayD-07 <abhaydeshmukh0001@gmail.com>
2025-12-29 23:33:27 +0530

    Added Two Sum solution
    Implement Two Sum solution using HashMap.

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,26 @@
+// Problem: Two Sum
+// Platform: LeetCode
+// Difficulty: Easy
+// Time: O(n)
+// Space: O(n)
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            
+            map.put(nums[i], i);
+        }
+        
+        // This line only executes if no pair is found (won’t happen per problem)
+        return new int[0];
+    }
+}
